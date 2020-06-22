5d3abef6273a307cfee2372179364c3d455e4a88
Hoang Phan <phan.minhhoang2606@gmail.com>
2020-06-21 22:24:09 -0400

    Added github pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route, Switch, Redirect } from 'react-router-dom';
+import {Route, Switch, Redirect, HashRouter} from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 import NewStoryPage from './Pages/NewStoryPage/NewStoryPage';
 import StoriesPage from './Pages/StoriesPage/StoriesPage';
@@ -10,7 +10,7 @@ import StoriesPage from './Pages/StoriesPage/StoriesPage';
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <HashRouter basename='/'>
         <Route path='/home' component={HomePage}/>
         <Route path='/stories' render={
           (props) => (<StoriesPage key={props.location.search} {...props}></StoriesPage>)
@@ -18,7 +18,7 @@ function App() {
         <Route path='/'>
           <Redirect to='/home'/>
         </Route>
-      </Switch>
+      </HashRouter>
     </div>
   );
 }
